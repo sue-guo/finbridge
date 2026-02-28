cb3ce0e9626c303c7d95549547183aceeda77f05
Hongxiu Guo <hxiuguo@gmail.com>
2026-02-27 23:57:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# finbridge
+This Repo - FinBridge – is a Financial Data Integration &amp; BI Platform Built with Spring Boot, Apache Camel
---
+++ src/test/java/io/github/sueguo/finbridge/FinbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sueguo.finbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinbridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
